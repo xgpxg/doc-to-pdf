85c2ea63f7046386e834fe6fed4df09994481088
wxg <1584929962@qq.com>
2026-01-28 18:04:35 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use anyhow::bail;
+use libreoffice_rs::Office;
+use libreoffice_rs::urls;
+use std::path::Path;
+use std::{env, fs};
+
+pub fn set_libreoffice_root_dir(dir: &str) -> anyhow::Result<()> {
+    unsafe {
+        env::set_var("LIBREOFFICE_ROOT_DIR", dir);
+    }
+    Ok(())
+}
+/// 将文件转换为 pdf
+pub fn convert(src: &str, dest: &str) -> anyhow::Result<()> {
+    let ext = Path::new(src).extension();
+    if ext.is_none() {
+        bail!("Invalid filename");
+    }
+    let ext = ext.unwrap().to_str().unwrap();
+    if ext != "docx"
+        && ext != "doc"
+        && ext != "xlsx"
+        && ext != "xls"
+        && ext != "pptx"
+        && ext != "ppt"
+        && ext != "csv"
+    {
+        bail!("Unsupported file format: {}", ext);
+    }
+
+    // 由于libreoffice路径似乎不支持中文，所以先复制一个临时文件
+    let temp_dir = env::temp_dir().join("doc-to-pdf");
+    fs::create_dir_all(&temp_dir)?;
+    let temp = temp_dir.join(&format!("{}.{}", uuid::Uuid::new_v4().to_string(), ext));
+    fs::copy(src, &temp)?;
+
+    let libreoffice_root_dir = env::var("LIBREOFFICE_ROOT_DIR")?;
+    let program_dir = format!("{}/program", libreoffice_root_dir);
+
+    {
+        let mut office = Office::new(&program_dir)?;
+
+        let doc_url = urls::local_into_abs(&temp.display().to_string())?;
+
+        let mut doc = office.document_load(doc_url)?;
+
+        let ok = doc.save_as(dest, "pdf", None);
+        if !ok {
+            log::error!("save pdf file failed: {}", office.get_error());
+            bail!("save pdf file failed: {}", office.get_error());
+        }
+    }
+
+    fs::remove_file(temp)?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use anyhow::anyhow;
+use clap::Parser;
+use doc_to_pdf::{convert, set_libreoffice_root_dir};
+use rust_embed::Embed;
+use std::path::PathBuf;
+use std::{env, fs};
+
+#[derive(Embed)]
+#[folder = "resources/"]
+struct Asset;
+
+const LIBREOFFICE_RESOURCE: &str = "libreoffice.tar.gz";
+
+#[derive(Parser)]
+#[command(name = "doc-to-pdf", about = "Convert document files to PDF format")]
+struct Args {
+    /// Source file to convert, Support doc, docx, xls, xlsx, ppt, pptx, csv
+    #[arg(short, long, value_parser)]
+    input: PathBuf,
+
+    /// Destination PDF file
+    #[arg(short, long, value_parser)]
+    output: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let temp_dir = env::temp_dir();
+    let libreoffice_path = temp_dir.join("libreoffice");
+
+    if !fs::exists(&libreoffice_path)? {
+        let libreoffice = Asset::get(LIBREOFFICE_RESOURCE)
+            .ok_or(anyhow!("{} not found", LIBREOFFICE_RESOURCE))?;
+        fs::write(&temp_dir.join(LIBREOFFICE_RESOURCE), libreoffice.data)?;
+        fs::create_dir_all(&libreoffice_path)?;
+        std::process::Command::new("tar")
+            .arg("-xzf")
+            .arg(&temp_dir.join(LIBREOFFICE_RESOURCE))
+            .arg("-C")
+            .arg(&libreoffice_path)
+            .output()?;
+    }
+
+    set_libreoffice_root_dir(&libreoffice_path.display().to_string())?;
+
+    let args = Args::parse();
+
+    println!(
+        "Converting {} to {}",
+        args.input.display(),
+        args.output.display()
+    );
+    convert(
+        args.input.display().to_string().as_str(),
+        args.output.display().to_string().as_str(),
+    )?;
+    println!("✅ Done");
+
+    Ok(())
+}
